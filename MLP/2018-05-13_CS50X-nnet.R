f23ca968449d2fa5b4ab0eb62ee58375026a3baf
pgurazada <pavankumar.gurazada@gmail.com>
2018-05-14 09:13:50 +0400

    more runs

--- MLP/2018-05-13_CS50X-nnet.R
+++ MLP/2018-05-13_CS50X-nnet.R
@@ -16,29 +16,24 @@ y_train <- readRDS("MLP/cs50x_ytrain.rds")
 y_test <- readRDS("MLP/cs50x_ytest.rds")
 
 model <- keras_model_sequential() %>% 
-  layer_dense(units = 400, 
+  layer_dense(units = 500, 
               activation = 'relu', 
               input_shape = ncol(x_train)) %>% # layer 1
   
   layer_dropout(rate = 0.2) %>% 
   
-  layer_dense(units = 300, 
+  layer_dense(units = 250, 
               activation = 'relu') %>% # layer 2
   
   layer_dropout(rate = 0.2) %>% 
   
-  layer_dense(units = 200, 
+  layer_dense(units = 100, 
               activation = 'relu') %>% # layer 3
   
   layer_dropout(rate = 0.2) %>%
   
-  layer_dense(units = 100, 
-              activation = 'relu') %>% # layer 4
-  
-  layer_dropout(rate = 0.2) %>%
-  
   layer_dense(units = 50, 
-              activation = 'relu') %>% # layer 5
+              activation = 'relu') %>% # layer 4
   
   layer_dropout(rate = 0.2) %>%
   
@@ -46,7 +41,7 @@ model <- keras_model_sequential() %>%
               activation = 'sigmoid') # output
 
 compile(model, 
-        optimizer = optimizer_adam(decay = 0.1),
+        optimizer = optimizer_adam(lr = 0.005),
         loss = 'binary_crossentropy',
         metrics = c('accuracy'))
 
@@ -55,3 +50,4 @@ history <- fit(model, x_train, y_train,
                batch_size = 100, 
                validation_split = 0.2)
 
+k_clear_session()
