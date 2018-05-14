768d5223dbe56e75aa625fdf53ea83b61bc419e4
pgurazada <pavankumar.gurazada@gmail.com>
2018-05-14 15:12:00 +0400

    several runs and scaffolding for parameter space tuning

--- MLP/2018-05-13_CS50X-nnet.R
+++ MLP/2018-05-13_CS50X-nnet.R
@@ -15,39 +15,69 @@ x_test <- readRDS("MLP/cs50x_xtest.rds")
 y_train <- readRDS("MLP/cs50x_ytrain.rds")
 y_test <- readRDS("MLP/cs50x_ytest.rds")
 
+FLAGS <- flags(flag_integer("dense_units1", 1000),
+               flag_numeric("dropout1", 0.2),
+               flag_integer("dense_units2", 500),
+               flag_numeric("dropout2", 0.2),
+               flag_integer("dense_units3", 200),
+               flag_numeric("dropout3", 0.2),
+               flag_integer("dense_units4", 100),
+               flag_numeric("dropout4", 0.2),
+               flag_integer("epochs", 5),
+               flag_integer("batch_size", 100),
+               flag_numeric("learning_rate", 0.005))
+
 model <- keras_model_sequential() %>% 
-  layer_dense(units = 500, 
-              activation = 'relu', 
+  layer_dense(units = FLAGS$dense_units1, 
+              activation = 'relu',
               input_shape = ncol(x_train)) %>% # layer 1
   
-  layer_dropout(rate = 0.2) %>% 
-  
-  layer_dense(units = 250, 
+  layer_dropout(rate = FLAGS$dropout1) %>% 
+
+  layer_dense(units = FLAGS$dense_units2, 
               activation = 'relu') %>% # layer 2
   
-  layer_dropout(rate = 0.2) %>% 
+  layer_dropout(rate = FLAGS$dropout2) %>% 
   
-  layer_dense(units = 100, 
+  layer_dense(units = FLAGS$dense_units3, 
               activation = 'relu') %>% # layer 3
   
-  layer_dropout(rate = 0.2) %>%
+  layer_dropout(rate = FLAGS$dropout3) %>% 
   
-  layer_dense(units = 50, 
+  layer_dense(units = FLAGS$dense_units4, 
               activation = 'relu') %>% # layer 4
   
-  layer_dropout(rate = 0.2) %>%
+  layer_dropout(rate = FLAGS$dropout4) %>% 
   
   layer_dense(units = 1, 
               activation = 'sigmoid') # output
 
 compile(model, 
-        optimizer = optimizer_adam(lr = 0.005),
+        optimizer = optimizer_adam(lr = FLAGS$learning_rate),
         loss = 'binary_crossentropy',
         metrics = c('accuracy'))
 
 history <- fit(model, x_train, y_train,
-               epochs = 5,
-               batch_size = 100, 
+               epochs = FLAGS$epochs,
+               batch_size = FLAGS$batch_size,
+               verbose = 1,
                validation_split = 0.2)
 
 k_clear_session()
+
+# tuning_run("MLP/2018-05-13_CS50X-nnet.R",
+#            runs_dir = "mlp_tuning",
+#            sample = 0.01,
+#            flags = list(dense_units1 = c(1000, 500),
+#                         dense_units2 = c(500, 400),
+#                         dense_units3 = c(200, 100),
+#                         dense_units4 = c(100, 50),
+#                         dropout1 = c(0.1, 0.2, 0.3),
+#                         dropout2 = c(0.1, 0.2, 0.3),
+#                         dropout3 = c(0.1, 0.2, 0.3),
+#                         dropout4 = c(0.1, 0.2, 0.3),
+#                         epochs = c(3, 5),
+#                         batch_size = c(100, 200),
+#                         learning_rate = c(0.001, 0.005)))
+
+# ls_runs(order = eval_acc, runs_dir = "mlp_tuning")
