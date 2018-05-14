b407fc5066032b04aba4524e7418ac97339ad2fd
pgurazada <pavankumar.gurazada@gmail.com>
2018-05-14 13:08:21 +0400

    compute ROC AUC for the final model

---
+++ MLP/2018-05-13_CS50X-roc.R
@@ -0,0 +1,81 @@
+#' ---
+#' title: "ROC curves for CS50x MLP"
+#' author: Pavan Gurazada
+#' output: github_document
+#' ---
+#' last update: Mon May 14 12:58:10 2018
+
+library(keras)
+library(yardstick)
+library(pROC)
+
+#' *Building the MLP*
+
+x_train <- readRDS("MLP/cs50x_xtrain.rds")
+x_test <- readRDS("MLP/cs50x_xtest.rds")
+
+y_train <- readRDS("MLP/cs50x_ytrain.rds")
+y_test <- readRDS("MLP/cs50x_ytest.rds")
+
+model <- keras_model_sequential() %>% 
+  layer_dense(units = 500, 
+              activation = 'relu', 
+              input_shape = ncol(x_train)) %>% # layer 1
+  
+  layer_dropout(rate = 0.2) %>% 
+  
+  layer_dense(units = 250, 
+              activation = 'relu') %>% # layer 2
+  
+  layer_dropout(rate = 0.2) %>% 
+  
+  layer_dense(units = 100, 
+              activation = 'relu') %>% # layer 3
+  
+  layer_dropout(rate = 0.2) %>%
+  
+  layer_dense(units = 50, 
+              activation = 'relu') %>% # layer 4
+  
+  layer_dropout(rate = 0.2) %>%
+  
+  layer_dense(units = 1, 
+              activation = 'sigmoid') # output
+
+compile(model, 
+        optimizer = optimizer_adam(lr = 0.005),
+        loss = 'binary_crossentropy',
+        metrics = c('accuracy'))
+
+history <- fit(model, x_train, y_train,
+               epochs = 5,
+               batch_size = 100, 
+               validation_split = 0.2)
+
+#' *Computing the ROC*
+
+predict_classes(object = model, 
+                x = x_test) %>% 
+  as.vector() ->
+  yhat_class
+
+predict_proba(object = model,
+              x = x_test) %>% 
+  as.vector() ->
+  yhat_probs
+
+estimates_test <- data.frame(truth = as.factor(y_test),
+                             estimate = as.factor(yhat_class),
+                             class_prob = yhat_probs)
+
+conf_mat(estimates_test, truth, estimate)
+metrics(estimates_test, truth , estimate)
+roc_auc(estimates_test, truth, class_prob)
+
+#' Plotting the ROC curve
+
+dev.new()
+plot(roc(estimates_test$truth, estimates_test$class_prob))
+
+#' The above plot shows that the model is definitely much better than random 
+#' guessing 
