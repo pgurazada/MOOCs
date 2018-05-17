c8ba07f8ae0443b34b2fbe391d5c0a621fd60b81
pgurazada <pavankumar.gurazada@gmail.com>
2018-05-17 11:08:01 +0400

    tuning and munging changes

--- MLP/2018-05-13_CS50X-nnet.R
+++ MLP/2018-05-13_CS50X-nnet.R
@@ -16,36 +16,48 @@ y_train <- readRDS("MLP/cs50x_ytrain.rds")
 y_test <- readRDS("MLP/cs50x_ytest.rds")
 
 FLAGS <- flags(flag_integer("dense_units1", 1000),
-               flag_numeric("dropout1", 0.2),
-               flag_integer("dense_units2", 500),
+               flag_numeric("dropout1", 0.1),
+               flag_integer("dense_units2", 400),
                flag_numeric("dropout2", 0.2),
                flag_integer("dense_units3", 200),
-               flag_numeric("dropout3", 0.2),
-               flag_integer("dense_units4", 100),
-               flag_numeric("dropout4", 0.2),
-               flag_integer("epochs", 5),
+               flag_numeric("dropout3", 0.3),
+               flag_integer("dense_units4", 50),
+               flag_numeric("dropout4", 0.3),
+               flag_integer("epochs", 15),
                flag_integer("batch_size", 100),
-               flag_numeric("learning_rate", 0.005))
+               flag_numeric("learning_rate", 10^-6))
 
 model <- keras_model_sequential() %>% 
   layer_dense(units = FLAGS$dense_units1, 
               activation = 'relu',
+              kernel_initializer = 'glorot_normal',
               input_shape = ncol(x_train)) %>% # layer 1
+  
+  layer_batch_normalization() %>% 
   
   layer_dropout(rate = FLAGS$dropout1) %>% 
 
   layer_dense(units = FLAGS$dense_units2, 
-              activation = 'relu') %>% # layer 2
+              activation = 'relu',
+              kernel_initializer = 'glorot_normal') %>% # layer 2
+  
+  layer_batch_normalization() %>% 
   
   layer_dropout(rate = FLAGS$dropout2) %>% 
   
   layer_dense(units = FLAGS$dense_units3, 
-              activation = 'relu') %>% # layer 3
+              activation = 'relu',
+              kernel_initializer = 'glorot_normal') %>% # layer 3
+  
+  layer_batch_normalization() %>% 
   
   layer_dropout(rate = FLAGS$dropout3) %>% 
   
   layer_dense(units = FLAGS$dense_units4, 
-              activation = 'relu') %>% # layer 4
+              activation = 'relu',
+              kernel_initializer = 'glorot_normal') %>% # layer 4
+  
+  layer_batch_normalization() %>% 
   
   layer_dropout(rate = FLAGS$dropout4) %>% 
   
@@ -61,7 +73,9 @@ history <- fit(model, x_train, y_train,
                epochs = FLAGS$epochs,
                batch_size = FLAGS$batch_size,
                verbose = 1,
-               validation_split = 0.2)
+               validation_split = 0.2, 
+               callbacks = list(callback_early_stopping(patience = 2),
+                                callback_reduce_lr_on_plateau(patience = 2)))
 
 k_clear_session()
 
@@ -80,4 +94,5 @@ k_clear_session()
 #                         batch_size = c(100, 200),
 #                         learning_rate = c(0.001, 0.005)))
 
-# ls_runs(order = eval_acc, runs_dir = "mlp_tuning")
+# ls_runs(order = metric_val_acc, runs_dir = "mlp_tuning")
+# copy_run(ls_runs(metric_val_acc >= 0.6872), to = "best-runs")
